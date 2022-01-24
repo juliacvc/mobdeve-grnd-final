9ad2dfaf5c780baec0a1685e26c0f45e58d01437
GabrielDeLeon74 <gabrielvictordeleon@gmail.com>
2022-01-24 22:55:24 +0800

    Added stuff

--- app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/main_nav/TimerFragment.kt
+++ app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/main_nav/TimerFragment.kt
@@ -1,24 +1,94 @@
 package com.mobdeve.s11.mco2.deleon.coronel.grnd.main_nav
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import com.mobdeve.s11.mco2.deleon.coronel.grnd.R
+import com.mobdeve.s11.mco2.deleon.coronel.grnd.databinding.FragmentTimerBinding
+import kotlin.math.roundToInt
 
 class TimerFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentTimerBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false)
+        binding = FragmentTimerBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //INSERT CODE HERE
+        binding.startStopButton.setOnClickListener {startStopTimer()}
+        binding.resetButton.setOnClickListener {resetTimer()}
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+
+        return view
     }
+
+    private fun resetTimer()
+    {
+        stopTimer()
+        time = 0.0
+        binding.timeTV.text = getTimeStringFromDouble(time)
+    }
+
+    private fun startStopTimer()
+    {
+        if(timerStarted)
+            stopTimer()
+        else
+            startTimer()
+    }
+
+    private fun startTimer()
+    {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
+        startService(serviceIntent)
+        binding.startStopButton.text = "Stop"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_pause)
+        timerStarted = true
+    }
+
+    private fun stopTimer()
+    {
+        stopService(serviceIntent)
+        binding.startStopButton.text = "Start"
+        binding.startStopButton.icon = getDrawable(R.drawable.ic_play)
+        timerStarted = false
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent)
+        {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
+            binding.timeTV.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): CharSequence?
+    {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hour, min, sec)
+
+
 }
