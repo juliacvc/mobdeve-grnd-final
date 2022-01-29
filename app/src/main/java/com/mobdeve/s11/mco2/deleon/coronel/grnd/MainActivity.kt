fa4ef8562bcfa2816e48049f39e03bf7df326a65
Julia Coronel <juliacyrillecoronel@gmail.com>
2022-01-29 18:01:42 +0800

    Fixed profile drawer layout

--- app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.mobdeve.s11.mco2.deleon.coronel.grnd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.mobdeve.s11.mco2.deleon.coronel.grnd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +21,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val drawerlayout : DrawerLayout = binding.drawerlayout
+        val navview : NavigationView = binding.navView
+
+        binding.profileBtn.setOnClickListener{
+            drawerlayout.openDrawer(GravityCompat.START)
+        }
+
+        navview.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_profile -> Toast.makeText(
+                    applicationContext,
+                    "Clicked Profile",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_settings -> Toast.makeText(
+                    applicationContext,
+                    "clicked Settings",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_logout -> {
+                    var goToLoginActivity = Intent(applicationContext, LoginActivity::class.java)
+
+                    startActivity(goToLoginActivity)
+                    finish()
+                }
+            }
+
+            true
+        }
 
         val bottomNavigationView = binding.navbarMain
         val navController = findNavController(R.id.fragment)
