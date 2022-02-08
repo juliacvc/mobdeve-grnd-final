359fb336dd658f95f8068321e3412d5a6c11ab5c
GabrielDeLeon74 <gabrielvictordeleon@gmail.com>
2022-02-08 18:56:33 +0800

    Created the card that will hold the recycler view

--- app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/adapters/CategoryAdapter.kt
@@ -3,9 +3,13 @@ package com.mobdeve.s11.mco2.deleon.coronel.grnd.adapters
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.mobdeve.s11.mco2.deleon.coronel.grnd.databinding.WorkoutsListRowBinding
+import com.mobdeve.s11.mco2.deleon.coronel.grnd.main_nav.CustomDialogFragment
 import com.mobdeve.s11.mco2.deleon.coronel.grnd.models.CategoryModel
+
 
 class CategoryAdapter(private val workoutList: ArrayList<CategoryModel?>): RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
     inner class ViewHolder(private val itemBinding: WorkoutsListRowBinding): RecyclerView.ViewHolder(itemBinding.root) {
@@ -21,6 +25,15 @@ class CategoryAdapter(private val workoutList: ArrayList<CategoryModel?>): Recyc
                 itemBinding.workoutItem.setBackgroundColor(Color.parseColor("#FFFFFF"))
                 itemBinding.workoutItem.setTextColor(Color.parseColor("#000000"))
             }
+            itemBinding.workoutItem.setOnClickListener{
+                var dialog = CustomDialogFragment()
+
+                //This is the code that doesnt work for some god forsaken reason
+//                dialog.show(supportFragmentManager, "customDialog")
+
+                Snackbar.make(itemView, "${item.name}", Snackbar.LENGTH_SHORT).show()
+            }
+
         }
     }
 
---
+++ app/src/main/java/com/mobdeve/s11/mco2/deleon/coronel/grnd/main_nav/CustomDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.mobdeve.s11.mco2.deleon.coronel.grnd.main_nav
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mobdeve.s11.mco2.deleon.coronel.grnd.R
+import com.mobdeve.s11.mco2.deleon.coronel.grnd.databinding.ActivityLoginBinding.inflate
+
+class CustomDialogFragment: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView: View = inflater.inflate(R.layout.custom_popup_dialogue, container, false)
+
+        return rootView
+    }
+
+}
